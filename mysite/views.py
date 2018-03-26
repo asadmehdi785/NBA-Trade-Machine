f66d149ea5e2bf82f6b21daa8529b4fcf0946e2d
Samuel Fok <foks@rpi.edu>
2018-03-26 18:10:31 -0400

    Moving in the files
    Github is up to date, please sync then pull

---
+++ mysite/views.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+def index(request):
+	return render(request, 'templates/index.html')
+
+def trade(request):
+	return render(request, 'templates/trade.html')
