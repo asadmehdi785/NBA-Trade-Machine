4eb5cb626e36d5647a1d4c6d48639ab4e3e23ed3
Asad <mehdia@rpi.edu>
2018-04-18 16:58:59 -0400

    Adding support for backend, some frontend design changes

--- mysite/static/js/trade.js
+++ mysite/static/js/trade.js
@@ -9,11 +9,20 @@ $(document).ready(function() {
 		var dropdown2text = $('#dropdownMenuButton2').text()
 		var dropdown3text = $('#dropdownMenuButton3').text()
 
-
-
 		if (dropdown1text !== 'Choose team' && dropdown2text !== 'Choose team'
 			|| dropdown2text !== 'Choose team' && dropdown3text !== 'Choose team'
 			|| dropdown1text !== 'Choose team' && dropdown3text !== 'Choose team') {
+
+			if(dropdown1text===dropdown2text){
+				//if  any two  dropdown contains same team then no action
+				return ;
+			}else if(dropdown2text===dropdown3text){
+				//if  any two  dropdown contains same team then no action
+				return ;
+			}else if(dropdown1text===dropdown3text){
+				//if  any two  dropdown contains same team then no action
+				return ;
+			}
 
 			// We bring the "Trade Options" part up here
 			var team1Div = $('#team1');
@@ -45,80 +54,60 @@ $(document).ready(function() {
 					}
 				}
 			});
-			$("#teamplayers2").click(function(event) {
-				var player = event.target;
-				$(player).appendTo("#team1tradeoptions div");
+			// $("#teamplayers2").click(function(event) {
+			// 	var player = event.target;
+			// 	$(player).appendTo("#team1tradeoptions div");
 
-				var allteam1options = $("#team1tradeoptions div").children();
-				var allteam2options = $("#team2tradeoptions div").children();
+			// 	var allteam1options = $("#team1tradeoptions div").children();
+			// 	var allteam2options = $("#team2tradeoptions div").children();
 
-				if (allteam1options.length === 1 && allteam2options.length === 1) {
-					team1options_playername = $(allteam1options[0]).text();
-					team2options_playername = $(allteam2options[0]).text();
+			// 	if (allteam1options.length === 1 && allteam2options.length === 1) {
+			// 		team1options_playername = $(allteam1options[0]).text();
+			// 		team2options_playername = $(allteam2options[0]).text();
 
-					if (team1options_playername === "Daniel Theis" && team2options_playername === "Kent Bazemore") {
-						$("#testtradeNotSuccessful").toggleClass('d-none d-block');
-						$("#testtrade").toggleClass('d-block d-none');
-					}
-				}
-			});
+			// 		if (team1options_playername === "Daniel Theis" && team2options_playername === "Kent Bazemore") {
+			// 			$("#testtradeNotSuccessful").toggleClass('d-none d-block');
+			// 			$("#testtrade").toggleClass('d-block d-none');
+			// 		}
+			// 	}
+			// });
 
-			// This handles clicking a player in "Trade Options" and puts them back
-			$("#team1tradeoptions div").click(function(event) {
-				var player = event.target;
-				$("#teamplayers2").prepend(player);
-			});
-			$("#team2tradeoptions div").click(function(event) {
-				var player = event.target;
-				$("#teamplayers1").prepend(player);
-			});
+			// // This handles clicking a player in "Trade Options" and puts them back
+			// $("#team1tradeoptions div").click(function(event) {
+			// 	var player = event.target;
+			// 	$("#teamplayers2").prepend(player);
+			// });
+			// $("#team2tradeoptions div").click(function(event) {
+			// 	var player = event.target;
+			// 	$("#teamplayers1").prepend(player);
+			// });
 
-			// This is just putting the Test Trade button
-			$("#testtrade").toggleClass('d-none d-block');
+			// // This is just putting the Test Trade button
+			// $("#testtrade").toggleClass('d-none d-block');
 		}
 	};
-	
-	// Arbitrary arrays of player names
-	var team1players = ['Kent Bazemore', "DeAndre' Bembry", 'Tyler Cavanaugh', 'Antonius Cleveland', 'John Collins',
-					    'Dewayne Dedmon', 'Malcolm Delaney', 'Tyler Dorsey', 'Damion Lee', 'Josh Magette',
-					    'Jaylen Morris', 'Mike Muscala', 'Miles Plumlee', 'Taurean Prince', 'Dennis Schroder',
-					    'Isaiah Taylor', 'Andrew White III'];
-	var team2players = ['Kyrie Irving', 'Jaylen Brown', 'Jayson Tatum', 'Jayson Tatum', 'Marcus Morris',
-					    'Al Horford', 'Terry Rozier', 'Marcus Smart', 'Greg Monroe', 'Aron Baynes',
-					    'Daniel Theis', 'Shane Larkin', 'Abdel Nader', 'Semi Ojeleye', 'Gordon Hayward',
-					    'Guerschon Yabusele', 'Jabari Bird'];
-	var team3players = ['Quincy Acy', 'Jarrett Allen', 'DeMarre Carroll', 'Allen Crabbe','Dante Cunningham',
-						'Spencer Dinwiddie', 'Milton Doyle', 'Joe Harris', 'Rondae Hollis-Jefferson', 'Caris LeVert',
-						'Jeremy Lin', 'Timofey Mozgov', 'Jahlil Okafor', "D'Angelo Russell", 'Nik Stauskas',
-						'James Webb III', '	Isaiah Whitehead'];
+
 
 	// This handles what happens when each dropdown is clicked			
-	$('#team11').click(function() {
-		$('#dropdownMenuButton1').text('Atlanta Hawks');
-		// We go through every player in Team 1, and add them in the list
-		for (i = 0; i < team1players.length; i++) {
-			$("#teamplayers1").append('<button id="player' + (i + 11) + '" type="button" class="list-group-item list-group-item-action">' + team1players[i] + '</button>')
-		}
-		readyToTrade();
-	});
-	// Same stuff here
-	$('#team22').click(function() {
-		$('#dropdownMenuButton2').text('Boston Celtics');
-		for (i = 0; i < team2players.length; i++) {
-			$("#teamplayers2").append('<button id="player' + (i + 21) + '" type="button" class="list-group-item list-group-item-action">' + team2players[i] + '</button>')
-		}
-		readyToTrade();
-	});
-	$('#team33').click(function() {
-		$('#dropdownMenuButton3').text('Brooklyn Nets');
-		for (i = 0; i < team3players.length; i++) {
-			$("#teamplayers3").append('<button id="player' + (i + 31) + '" type="button" class="list-group-item list-group-item-action">' + team3players[i] + '</button>')
-		}
-		readyToTrade();
-	});
+	$('.team1dropdown').click(function() {
 
-	$('#addteam3').click(function() {
-		$("#team3").toggleClass('d-none d-block');
-		$("#addteam3").toggleClass('d-block d-none');
+		// This makes the dropdown button have the text of the team
+		var teamSelected = $(this).text();
+		$('#dropdownMenuButton1').text(teamSelected);
+		readyToTrade();
+	});
+	$('.team2dropdown').click(function() {
+
+		// This makes the dropdown button have the text of the team
+		var teamSelected = $(this).text();
+		$('#dropdownMenuButton2').text(teamSelected);
+		readyToTrade();
+	});
+	$('.team3dropdown').click(function() {
+
+		// This makes the dropdown button have the text of the team
+		var teamSelected = $(this).text();
+		$('#dropdownMenuButton3').text(teamSelected);
+		readyToTrade();
 	});
 });
--- mysite/urls.py
+++ mysite/urls.py
@@ -11,7 +11,12 @@ urlpatterns = [
     url(r'^logout/$', auth_views.logout, {'next_page': 'home'}, name='logout'),
     url(r'^signup/$', core_views.signup, name='signup'),
 	url(r'^trade/$', core_views.trade, name='trade'),
-	url(r'^result/$', core_views.result, name='result'),
+	url(r'^test_trade/$', core_views.test_trade, name='test_trade'),
 	url(r'^failure/$', core_views.failure, name='failure'),
+    url(r'^success/$', core_views.success, name='success'),
 	url(r'^admin/', admin.site.urls),
+    url(r'^get_player_by_team',core_views.get_player_by_team),
+    url(r'^get_player_info', core_views.get_player_info),
+    url(r'^get_player_stuff_test', core_views.get_player_stuff_test)
+
 ]
